b21fe01db2c41007fccc80d35294dc912fdb96fb
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-11-08 20:25:53 +0500

    changes 51

--- index.js
+++ index.js
@@ -23,203 +23,47 @@ mongoose.connect(MONGODB_URI, {
 });
 
 
-const noteSchema = new mongoose.Schema({
-  subject: String,
-  chapterNo: Number,
-  chapterName: String,
-  text: String,
-  title: String
-  
+const imageSchema = new mongoose.Schema({
+  url: String
 });
+const Image = mongoose.model('Image', imageSchema);
 
-const Note = mongoose.model('Note', noteSchema);
+// Configure multer for file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-app.post('/notes', async (req, res) => {
-  try {
-      const { subject, chapterNo, chapterName, text , title } = req.body;
-      // Check if the note already exists with the same subject, chapterNo, and chapterName
-      const existingNote = await Note.findOne({ subject, chapterNo, chapterName });
-      if (existingNote) {
-          // If the note exists, update the existing record
-          existingNote.text = text;
-          await existingNote.save();
-          return res.status(200).json({
-              success: true,
-              message: 'Data updated successfully.',
-          });
-      } else {
-          // If the note doesn't exist, create a new record
-          const newNote = new Note({
-              subject,
-              chapterNo,
-              chapterName,
-              text,
-              title
-          });
-          await newNote.save();
-          return res.status(201).json({
-              success: true,
-              message: 'Data saved successfully.',
-          });
-      }
-  } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-          success: false,
-          message: 'Failed to save data.',
-          error: error,
-      });
-  }
-});
+// Define route for image upload
+app.post('/upload', upload.single('image'), async (req, res) => {
+  const formData = new FormData();
+  formData.append('image', req.file.buffer.toString('base64'));
 
-
-
-
-app.get('/notes', async (req, res) => {
-    try {
-      const experiences = await Note.find();
-      res.json(experiences);
-    } catch (error) {
-      alert(error);
-      res.status(500).json({
-        success: false,
-        message: 'Failed to fetch.',
-        error: error,
-      });
-    }
+  const response = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
+      method: 'POST',
+      body: formData,
   });
 
-  
+  const data = await response.json();
 
+  const imageUrl = data.data.url;
 
-  
-app.put('/notes/:id', async (req, res) => {
-    const { id } = req.params;
-    const { subject, chapterNo, chapterName, text  , title} = req.body;
-  
-    try {
-      const updatedExperience = await Note.findByIdAndUpdate(id, {
-        subject : subject,
-         chapterNo : chapterNo,
-         chapterName : chapterName,
-          text:text,
-          title:title
-      }, { new: true });
-  
-      res.json(updatedExperience);
-    } catch (error) {
-      console.error('Error updating experience', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-  
-  
+  // Save the image URL to the database
+  const newImage = new Image({ url: imageUrl });
+  await newImage.save();
 
-
-
-// Delete an experience
-app.delete('/notes/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      await Note.findByIdAndDelete(id);
-  
-      res.json({ message: 'Experience deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting experience', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
-  
-  
-
-
-
-
-
-
-
-// code for construction sending emails form
-
-
-app.post("/send-data", (req, res) => {
-  
-  const {  name , email , subject , message} = req.body;
-  console.log(name);
-  
-  const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: 'vascularbundle43@gmail.com',
-    pass: 'gxauudkzvdvhdzbg',
-  },
+  res.redirect('/');
 });
 
-const storeMailOptions = {
-  from: email,
-  to: "vascularbundle43@gmail.com",
-  subject: `New Message from ${name} through Notes App`,
-  html: `
-    <center><h2 style="color: #c8a97e;">Great News! <br> <span style="color:#212529">You have received a message from ${name}</span></h2></center>
-    <hr style="border: 2px solid #c8a97e;">
-    <center><h3>Message</h3></center>
-    <p> ${message}</p>
-    <p style="margin-top:10px;">Contact: ${email}</p>
-    
-    
-    `,
-};
-const userMailOptions = {
-  from: "vascularbundle43@gmail.com",
-  to: email,
-  subject: subject,
-  html: `
-    <center><h2>Welcome to Usman Kashif Notes & Resources,<br><br> <center><span style="color: #c8a97e;">${name}!</span></center></h2></center>
-    <p>Thank you for reaching put to us. Here is your message:</p>
-    <center><h3>Message</h3></center>
-    <p> ${message}</p>
-    
-    <p>Your email has been received and will be processed shortly. Thank you for Us!</p>
-  `,
-};
-
-// Send the email to the store
-transporter.sendMail(storeMailOptions, function(error, storeInfo) {
-  if (error) {
-    console.error(error);
-    res.status(500).send("Error sending email to store");
-  } else {
-    console.log("Email sent to store: " + storeInfo.response);
-
-    // Send the email to the user
-    transporter.sendMail(userMailOptions, function(error, userInfo) {
-      if (error) {
-        console.error(error);
-        res.status(500).send("Error sending email to user");
-      } else {
-        console.log("Email sent to user: " + userInfo.response);
-        res.status(200).send("Order submitted successfully");
-      }
-    });
-  }
+// Define route for fetching images
+app.get('/images', async (req, res) => {
+  const images = await Image.find();
+  res.send(images);
 });
-
-});
-
-
-
-
 
 
 
 
 app.get("/", (req,res) =>{
-  res.send("Backend server has started running successfully...");
+  res.send("Backend server for blogs images has started running successfully...");
 });
 
 
