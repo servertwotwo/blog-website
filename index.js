68e082536a3677678154c83ac7e0de0b64485629
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2023-11-08 20:43:31 +0500

    five5

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 const FormData = require('form-data');
 
-
 app.use(express.json());
 app.use(cors());
-
-
 
 const PORT = 3000;
 
@@ -19,13 +16,11 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 
 const imageSchema = new mongoose.Schema({
   url: String
@@ -39,10 +34,10 @@ const upload = multer({ storage: storage });
 // Define route for image upload
 app.post('/upload', upload.single('image'), async (req, res) => {
   const formData = new FormData();
-  const fetch = await import('node-fetch');
+  const { default: fetch } = await import('node-fetch');
   formData.append('image', req.file.buffer.toString('base64'));
 
-  const response = await fetch.default('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
+  const response = await fetch('https://api.imgbb.com/1/upload?key=368cbdb895c5bed277d50d216adbfa52', {
     method: 'POST',
     body: formData,
   });
@@ -64,16 +59,10 @@ app.get('/images', async (req, res) => {
   res.send(images);
 });
 
-
-
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
   res.send("Backend server for blogs images has started running successfully...");
 });
 
-
-
-
 const server = app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-  });
-  
+  console.log(`Server started on port ${PORT}`);
+});
